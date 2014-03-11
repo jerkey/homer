6297a237fd71a403ea359f1b7d1f01b743bd2c0a
treeherder <b.d.reddybest@gmail.com>
2014-03-11 05:13:54 -0700

    working scripts

--- engine.py
+++ engine.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#set up globally scoped variables for telemetry 
+tool_home = {'x': 0.0, 'y': 0.0, 'z': 0.0}
+cam_home = {'x': 0.0, 'y': 0.0, 'z': 0.0}
+#these two dictionaries will store the arithmetic from each movement
+
 import curses
 import need
 from subprocess import Popen, PIPE, call
@@ -11,26 +16,84 @@ screen.keypad(1)  #nothing works without this
 
 screen.addstr("m g up down left right pgup pgdn increment c t\n")
 while True:
+
+  
   press = screen.getch()
   if press == ord("q"): break  #quit  ord values are important
+  
+
+''' these methods are for moving the tool head around
+they still need a scalar variable implementation'''
+
+
   elif press == curses.KEY_LEFT:  #this is pretty straightforward
     screen.clear()
     screen.addstr( "left")
-  elif press == curses.KEY_PPAGE:  #this is pretty straightforward
-    screen.clear()
-    screen.addstr( "pgup")
-  elif press == curses.KEY_NPAGE:  #this is pretty straightforward
-    screen.clear()
-    screen.addstr( "pgdn")
-  elif press == curses.KEY_RIGHT:  #this is pretty straightforward
+    ptr.xp()  #x axis plus
+    cam_home['x']+=0.1
+    tool_home['x']+=0.1  #this needs to be modular for scalar
+  elif press == curses.KEY_RIGHT: 
     screen.clear()
     screen.addstr( "right")
+    ptr.xm()  #x axis minus
+    cam_home['x']-=0.1
+    tool_home['x']-=0.1  #this needs to be modular for scalar
+  elif press == curses.KEY_UP:
+    screen.clear()
+    screen.addstr( "up")
+    cam_home['y']+=0.1
+    tool_home['y']+=0.1  #this needs to be modular for scalar
+    ptr.yp()
+  elif press == curses.KEY_DOWN:  
+    screen.clear()
+    screen.addstr( "down")
+    ptr.ym()
+    cam_home['y']-=0.1
+    tool_home['y']-=0.1  #this needs to be modular for scalar
+
+  elif press == curses.KEY_PPAGE:  
+    screen.clear()
+    screen.addstr( "pgup")
+    ptr.zp()
+    cam_home['z']+=0.1
+    tool_home['z']+=0.1  #this needs to be modular for scalar
+
+  elif press == curses.KEY_NPAGE: 
+    screen.clear()
+    screen.addstr( "pgdn")
+    ptr.zm()
+    cam_home['z']-=0.1
+    tool_home['z']-=0.1  #this needs to be modular for scalar
+
+
+'''these methods provide telemetry, orientation data for the user'''
+
+  elif press == ord("t"):
+    #these functions need a vector to track
+    screen.clear()   
+    screen.addstr("toolhead home")
+    for i in tool_home:
+      tool_home[i] = 0  #make this the starting point
+  elif press == ord("c"):
+    #these functions need a vector to track
+    screen.clear()   
+    screen.addstr("camera home")
+    for i in cam_home:
+      cam_home[i] = 0  #make this the starting point
+  
+'''these methods  allow the user to send raw g code to the printer'''
+
   elif press == ord("g"):
     screen.clear()   #otherwise things get messy
     moment = screen.getstr()
-    #ret = moment.split(" ")
-    #nret = "".join(ret)
     screen.addstr(moment)
-    #screen.refresh()
-    need.main(moment)
+    need.g(moment)
+  
+  elif press == ord("m"):
+    screen.clear()   #otherwise things get messy
+    moment = screen.getstr()
+    screen.addstr(moment)
+    need.m(moment)
+
+
 curses.endwin() #there's no place like home
--- ptr.py
+++ ptr.py
@@ -1,19 +1,67 @@
 import sys
 import serial
 from time import sleep
+
+
+
 class prntr:
-  def __init__(self ):
+  #this is the base class for all printer communication
+
+  def __init__(self ): #anything that needs to happen when the device connects
     self.com = serial.Serial('/dev/ttyACM0', 230400, timeout = 2)
-    sleep(2)
-  def wrk(self, *cmd):
+    sleep(8)  #wait for data
+    self.com.readlines()
+    self.com.flush()
+    self.com.write("G 91\r\n")  #everything in this script is for relative motion
+    return self.com.readlines()
+
+  def cmnd(self, *cmd):
+    com.flush()
     self.com.write(cmd)
-  def test(self):
-   self.com.write("M106 S255\n\r")
+    return self.com.readlines()
+
+  def fan(self):
+   self.com.write("M106 S255\r\n")
+  
+''' these methods are for controlling relative movement of the toolhead'''
+
+
+  def xp(self):  #eventually this is going to take a scalar type as well for an argument
+    self.com.flush()
+    self.com.write("G X 1\r\n")
+    return self.com.readlines()
+
+  def xm(self):  #P for PLUS M for MINUS
+    self.com.flush()
+    self.com.write("G X -1\r\n")
+    return self.com.readlines()
+
+  def yp(self):
+    self.com.flush()
+    self.com.write("G Y 1\r\n")
+    return self.com.readlines()
+
+  def ym(self): 
+    self.com.flush()
+    self.com.write("G Y -1\r\n")
+    return self.com.readlines()
+
+  def zp(self):
+    self.com.flush()
+    self.com.write("G Y 1\r\n")
+    return self.com.readlines()
+
+  def zm(self): 
+    self.com.flush()
+    self.com.write("G Y -1\r\n")
+    return self.com.readlines()
+
+
+initialize the routine
+bot = prntr()
+
 def main(*arg):
-  print "I moved the printer, G"+" "+" ".join(arg[1:])
-  bot = prntr()
-  sleep(6)
-  print bot.test()
+  print bot.cmd()
 
 
 if __name__=='__main__':
