b14bc7f6d9f2747819949fbc11f2772b05d66d04
jerkey <jerkey@halo.nu>
2014-08-17 09:16:38 -0700

    fix ptr.py to simply report failure if serialport won't open

--- ptr.py
+++ ptr.py
@@ -2,25 +2,26 @@ import sys
 import serial
 import time
 
-serialPort = 'testing' #'/dev/ttys001'
-#serialPort = '/dev/ttyACM0'
+serialPort = '/dev/ttyACM0'
 baudRate = 230400
 
 class prntr:
   #this is the base class for all printer communication
 
   def init(self ): #anything that needs to happen when the device connects
-    returnMsg = ""
-    if serialPort != 'testing':
+    try:
       self.com = serial.Serial(serialPort, baudRate, timeout = 1)
-      self.com.readlines()
-      self.com.write("G 91\r\n")  #everything in this script is for relative motion
-      if self.waitOk() != "":
-        returnMsg = returnMsg + "no 'ok' from printer after G 91"
-      self.com.write("M201 Z50\r\n")  #change the Z acceleration to prevent upward stripping
-      if self.waitOk() != "":
-        returnMsg = returnMsg + "no 'ok' from printer after M201 Z50"
-      if returnMsg == "": returnMsg = "ptr.init OK"
+    except:
+      return "fail to open printer"
+    self.com.readlines()
+    returnMsg = ""
+    self.com.write("G 91\r\n")  #everything in this script is for relative motion
+    if self.waitOk() != "":
+      returnMsg = returnMsg + "no 'ok' from printer after G 91"
+    self.com.write("M201 Z50\r\n")  #change the Z acceleration to prevent upward stripping
+    if self.waitOk() != "":
+      returnMsg = returnMsg + "no 'ok' from printer after M201 Z50"
+    if returnMsg == "": returnMsg = "ptr.init OK"
     return returnMsg
 
   def waitOk(self):
