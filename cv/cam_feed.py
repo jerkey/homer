f51d276ef9206d196eaeae5551150c09be4a0234
jerkey <jerkey@halo.nu>
2014-08-22 21:33:54 -0700

    simplify cam_feed.py in preparation for focus experiments

--- cv/cam_feed.py
+++ cv/cam_feed.py
@@ -1,21 +1,16 @@
 import cv2
 
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-  rval, frame = vc.read()
+if camera.isOpened(): # try to get the first frame
+  rval, frame = camera.read()
 else:
   rval = False
 
 while rval:
-  x = 310
-  y = 240
-  text_color = (255,0,0) # B,G,R
-  
-  cv2.putText(frame, "x", (x,y), cv2.FONT_HERSHEY_PLAIN, 4.0, text_color, thickness=3) 
   cv2.imshow("preview", frame)
-  rval, frame = vc.read()
+  rval, frame = camera.read()
   key = cv2.waitKey(20)
   if key == 27: # exit on ESC
     break
