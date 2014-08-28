0bbb6e83bbe33ad4e629980e54dfd5a4ce041cb1
jerkey <jerkey@halo.nu>
2014-08-27 20:30:34 -0700

    hist.py now shows entropy or focus value for the masked portion of the frame

--- cv/hist.py
+++ cv/hist.py
@@ -1,5 +1,7 @@
 import cv2, math, os
-import numpy as np
+import numpy
+
+focusWindowSize = 0.3 # the portion of the frame that is masked (center)
 
 os.environ["DISPLAY"] = ":0" # tell it to open the camera on the local machine even if we're remote
 cv2.namedWindow("preview")
@@ -14,18 +16,20 @@ cv2.imshow("preview", frame)
 rval, frame = camera.read()
 key = cv2.waitKey(20)
 
-while rval:
-  # create a mask
-  mask = np.zeros(frame.shape[:2], np.uint8)
-  mask[100:300, 100:400] = 255
-  masked_frame = cv2.bitwise_and(frame,frame,mask = mask)
+# create a focusWindow
+height, width = frame.shape[:2]
+focusWindow = numpy.zeros((height,width), numpy.uint8)
+vborder = (height*(1-focusWindowSize))/2
+hborder = (width*(1-focusWindowSize))/2
+focusWindow[vborder:height-vborder, hborder:width-hborder] = 255
 
+while rval:
   # Calculate histogram with mask and without mask
   # Check third argument for mask
   hist_full = cv2.calcHist([frame],[0],None,[256],[0,256])
-  hist_mask = cv2.calcHist([frame],[0],mask,[256],[0,256])
+  hist_focusWindow = cv2.calcHist([frame],[0],focusWindow,[256],[0,256])
 
-  histogram = [item for sublist in hist_full for item in sublist]
+  histogram = [item for sublist in hist_focusWindow for item in sublist]
 
   histogram_length = sum(histogram)
 
@@ -36,10 +40,12 @@ while rval:
   print entropy
 
   cv2.imshow("preview", frame)
+  # key = cv2.waitKey(20)
+  # cv2.imshow("preview", focusWindow) # show the focus window for comparison
   rval, frame = camera.read()
   key = cv2.waitKey(20)
 
-  if key == 27: # exit on ESC
+  if key in {27,32}: # exit on ESC or space
     break
   
 cv2.destroyWindow("preview")
