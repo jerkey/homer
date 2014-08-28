8ce46dd68136ecad7ab4db94c10428e1202cecee
jerkey <jerkey@halo.nu>
2014-08-27 19:39:51 -0700

    try again to get focus into hist.py

--- cv/hist.py
+++ cv/hist.py
@@ -1,6 +1,7 @@
-import cv2, math
-import numpy
+import cv2, math, os
+import numpy as np
 
+os.environ["DISPLAY"] = ":0" # tell it to open the camera on the local machine even if we're remote
 cv2.namedWindow("preview")
 camera = cv2.VideoCapture(0)
 
@@ -9,13 +10,13 @@ if camera.isOpened(): # try to get the first frame
 else:
   rval = False
 
-while rval:
-  cv2.imshow("preview", frame)
-  rval, frame = camera.read()
-  key = cv2.waitKey(20)
+cv2.imshow("preview", frame)
+rval, frame = camera.read()
+key = cv2.waitKey(20)
 
+while rval:
   # create a mask
-  mask = numpy.zeros(frame.shape[:2], numpy.uint8)
+  mask = np.zeros(frame.shape[:2], np.uint8)
   mask[100:300, 100:400] = 255
   masked_frame = cv2.bitwise_and(frame,frame,mask = mask)
 
@@ -23,12 +24,22 @@ while rval:
   # Check third argument for mask
   hist_full = cv2.calcHist([frame],[0],None,[256],[0,256])
   hist_mask = cv2.calcHist([frame],[0],mask,[256],[0,256])
-  print hist_mask
+
+  histogram = [item for sublist in hist_full for item in sublist]
+
+  histogram_length = sum(histogram)
+
+  samples_probability = [float(h) / histogram_length for h in histogram]
+
+  entropy = -sum([p * math.log(p, 2) for p in samples_probability if p != 0])
+
+  print entropy
+
+  cv2.imshow("preview", frame)
+  rval, frame = camera.read()
+  key = cv2.waitKey(20)
 
   if key == 27: # exit on ESC
     break
   
-  while key != 32:
-    key = cv2.waitKey(20)
-
 cv2.destroyWindow("preview")
